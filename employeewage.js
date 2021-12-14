57b0c80d59ffbf8062f77a9ee165894126db71ad
bolishettianjali <bolishettianjali@gamil.com>
2021-12-14 18:02:31 +0530

    Employee wage for 20 days

--- employeewage.js
+++ employeewage.js
@@ -13,6 +13,7 @@ const IS_FULL_TIME=2;
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOURS=20;
+const NUM_OF_WORKING_DAYS=20;
 
 function getWorkingHours(empCheck){
 switch(empCheck){
@@ -24,8 +25,11 @@ switch(empCheck){
         return 0;      
 }
 }
-
+let empHrs=0;
+for(let day=0;day<NUM_OF_WORKING_DAYS;day++){
 let empCheck=Math.floor(Math.random()*10)%3;
-let empHrs=getWorkingHours(empCheck);
+ empHrs+=getWorkingHours(empCheck);
+
+}
 let empWage=empHrs*WAGE_PER_HOURS;
 console.log("Emp wage:"+empWage);
